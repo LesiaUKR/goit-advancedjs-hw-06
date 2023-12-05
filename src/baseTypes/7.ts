20c3fa4d96751c8f00cbae8f9ab2dab2153a46a1
Lesia Soloviova <solovyova.lesya@gmail.com>
2023-12-05 22:44:15 +0200

    feat: solve baseType tasks

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,30 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum WeekDays {
+  monday = "Monday",
+  tuesday = "Tuesday",
+  wednesday = "Wednesday",
+  thursday = "Thursday",
+  friday = "Friday",
+  saturday = "Saturday",
+  sunday = "Sunday",
+}
+
+function isWeekend(weekDay: WeekDays): boolean {
+  switch (weekDay) {
+    case WeekDays.monday:
+    case WeekDays.tuesday:
+    case WeekDays.wednesday:
+    case WeekDays.thursday:
+    case WeekDays.friday:
+      return false;
+    case WeekDays.saturday:
+    case WeekDays.sunday:
+      return true;
+  }
+}
+
+const today: WeekDays = WeekDays.monday;
+console.log(`Is ${today} a weekend? ${isWeekend(today)}`);
--- src/baseTypes/8.ts
+++ src/baseTypes/8.ts
@@ -1,8 +1,13 @@
 /*
   Створіть тип "Gender", використовуючи union type, 
-  який може містити значення "male", "female". Створіть змінну myGender цього типу.
+  який може містити значення "male", "female". Створіть 
+  змінну myGender цього типу.
 */
 
-const myGender;
+type Gender = "male" | "female";
+let myGender: Gender;
 
-export {};
+myGender = "male";
+myGender = "female";
+
+// export {};
